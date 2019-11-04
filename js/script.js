56db7a2f6b61adc7323caa9d6e913e76114be2a2
yooonn <yns@hawaii.edu>
2019-11-04 00:30:29 -1000

    Added Home

--- js/script.js
+++ js/script.js
@@ -14,3 +14,29 @@ for (var i = 0, len = mysitenavlinks.length; i < len; i++) {
         mysitenav.classList.remove('active');
     };
 };
+
+
+/**
+ * This was built using the scrollie jQuery Plugin
+ * https://github.com/Funsella/jquery-scrollie
+ */
+
+
+$( window ).ready(function() {
+  
+    var wHeight = $(window).height();
+
+    $('.slide')
+      .height(wHeight)
+      .scrollie({
+        scrollOffset : -50,
+        scrollingInView : function(elem) {
+                   
+          var bgColor = elem.data('background');
+          
+          $('body').css('background-color', bgColor);
+          
+        }
+      });
+
+  });
